2d647f71dae3676924884325234fe2a948f7c2f1
Tahmid Mahmud <73707938+Tahmid-Mahmud@users.noreply.github.com>
2023-06-26 17:33:47 +0600

    B

--- birthday.js
+++ birthday.js
@@ -3,5 +3,6 @@ var dlist={
     	"B_5_6": "Minhazul Islam",
     	"B_1_10": "Kazi Farabi",
     	"B_21_1": "Nahian Al Mahi",
+	"B_26_6": "Likhon Sarkar Spondon",
     	"B_23_9": "Zuhair Anjum Ashfaq"
 }
